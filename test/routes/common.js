d6cc93326812f945a7e433e147f89e79d5ef38fd
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-05-08 23:16:14 -0400

    test-cleaning

--- test/routes/common.js
+++ test/routes/common.js
@@ -2,18 +2,21 @@
 
 exports.generateTestItem = function (obj, fn, args) {
     return function (done) {
+        if (args && !Array.isArray(args)) {
+            args = [args];
+        }
         var fnArgs = args ? args.concat([done]) : [done];
         fn.apply(obj, fnArgs);
     };
 };
 
 exports.putPatientRefs = function (resourceSets, patients, patientProperty) {
-	resourceSets.forEach(function(resources, index) {
-		var reference = patients[index].id;
-		resources.forEach(function(resource) {
-			resource[patientProperty] = {
-				reference: reference
-			}
-		});
-	});
-}
+    resourceSets.forEach(function (resources, index) {
+        var reference = patients[index].id;
+        resources.forEach(function (resource) {
+            resource[patientProperty] = {
+                reference: reference
+            };
+        });
+    });
+};
--- test/routes/test-allergyIntolerance.js
+++ test/routes/test-allergyIntolerance.js
@@ -26,39 +26,35 @@ describe('routes allergyIntolerance', function () {
 
     var samplesSets = [samples.set0(), samples.set1()];
 
-    var mgr = stri.sampleSetAdapter(samplesSets);
-    var ptMgr = ptStri.sampleSetAdapter([patientSamples]);
-
     before(itFn(app, app.start));
 
     it('check config (inits database as well)', itFn(stri, stri.getConfig));
 
     it('clear database', itFn(app, app.cleardb));
 
-    it('create with patient missing', itFn(mgr, mgr.createNegative, [0, 0]));
+    it('create with patient missing', itFn(stri, stri.createNegative, samplesSets[0][0]));
 
-    _.range(2).forEach(function(index) {
-        it('create patient ' + index, itFn(ptMgr, ptMgr.create, [0, index]));
+    _.range(2).forEach(function (index) {
+        it('create patient ' + index, itFn(ptStri, ptStri.create, [patientSamples[index]]));
     }, this);
 
     it('assign patients to samples', function () {
-        common.putPatientRefs([samplesSets[0], samplesSets[1]], patientSamples, 'patient');
+        common.putPatientRefs(samplesSets, patientSamples, 'patient');
     });
 
-    _.range(2).forEach(function(i) {
-        _.range(samplesSets[i].length).forEach(function(j) {
-            it('create for patient-' + i + ' ' + j, itFn(mgr, mgr.create, [i, j])); 
+    _.range(2).forEach(function (i) {
+        _.range(samplesSets[i].length).forEach(function (j) {
+            it('create for patient-' + i + ' ' + j, itFn(stri, stri.create, samplesSets[i][j]));
         }, this);
     }, this);
-
 
     var n = samplesSets[0].length + samplesSets[1].length;
     it('search (get - no param)', itFn(stri, stri.search, [n, {}]));
     it('search (post - no param)', itFn(stri, stri.searchByPost, [n, {}]));
 
-    _.range(2).forEach(function(i) {
-        _.range(samplesSets[i].length).forEach(function(j) {
-            it('read for patient-' + i + ' ' + j, itFn(mgr, mgr.read, [i, j])); 
+    _.range(2).forEach(function (i) {
+        _.range(samplesSets[i].length).forEach(function (j) {
+            it('read for patient-' + i + ' ' + j, itFn(stri, stri.read, samplesSets[i][j]));
         }, this);
     }, this);
 
@@ -67,17 +63,17 @@ describe('routes allergyIntolerance', function () {
         samplesSets[1][0].recordedDate = '2003-05-05';
     });
 
-    _.range(2).forEach(function(i) {
-        it('detect updated not equal db for patient-' + i, itFn(mgr, mgr.readNegative, [i, 0]));
-        it('update for patient-' + i, itFn(mgr, mgr.update, [i, 0]));
-        it('read for patient-' + i, itFn(mgr, mgr.read, [i, 0]));
-    });
+    _.range(2).forEach(function (i) {
+        it('detect updated not equal db for patient-' + i, itFn(stri, stri.readNegative, samplesSets[i][0]));
+        it('update for patient-' + i, itFn(stri, stri.update, samplesSets[i][0]));
+        it('read for patient-' + i, itFn(stri, stri.read, samplesSets[i][0]));
+    }, this);
 
     var n0 = samplesSets[0].length - 1;
     var n1 = samplesSets[1].length - 1;
 
-    it('delete last for patient-0', itFn(mgr, mgr.delete, [0, n0]));
-    it('delete last for patient-1', itFn(mgr, mgr.delete, [1, n1]));
+    it('delete last for patient-0', itFn(stri, stri.delete, samplesSets[0][n0]));
+    it('delete last for patient-1', itFn(stri, stri.delete, samplesSets[1][n1]));
 
     it('search (no param)', itFn(stri, stri.search, [n0 + n1, {}]));
 
